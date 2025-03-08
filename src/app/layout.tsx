125acfbbfcd29cdc4dd36a870109fa19a448d361
João Veloso <j.veloso.00@gmail.com>
2025-03-08 21:00:31 +0000

    Add vercel/analytics
    Add vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import StructuredData from './structured-data'
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
